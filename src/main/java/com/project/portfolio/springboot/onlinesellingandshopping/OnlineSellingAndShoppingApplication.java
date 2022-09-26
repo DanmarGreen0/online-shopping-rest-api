96341c2f5819e804232bc38ca46e4222b0f0cde1
Danmar <danmargreen0@gmail.com>
2022-09-26 13:55:04 -0700

    create spring boot web project

---
+++ src/main/java/com/project/portfolio/springboot/onlinesellingandshopping/OnlineSellingAndShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.project.portfolio.springboot.onlinesellingandshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSellingAndShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSellingAndShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/portfolio/springboot/onlinesellingandshopping/OnlineSellingAndShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.portfolio.springboot.onlinesellingandshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSellingAndShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
