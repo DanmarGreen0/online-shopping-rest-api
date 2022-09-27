c0e330901c2096a12192a76e8d29f4a54e64a0e1
Danmar <danmargreen0@gmail.com>
2022-09-26 17:42:37 -0700

    test

--- src/test/java/com/project/portfolio/springboot/onlinesellingandshopping/OnlineSellingAndShoppingApplicationTests.java
+++ src/test/java/com/project/portfolio/springboot/onlinesellingandshopping/OnlineSellingAndShoppingApplicationTests.java
@@ -1,13 +1,17 @@
 package com.project.portfolio.springboot.onlinesellingandshopping;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineSellingAndShoppingApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+
 	}
 
 }
